c42aabe7e3f98a1e7d05bdadf0d896633d55fecc
Rica Duchateau <rica.duchateau@informed.com>
2018-10-25 16:27:36 +0100

    Add routing

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/vehicle-checker/local-authority', function (req, res) {
+    var localAuthority = req.body['local-authority'];
+
+    if (localAuthority == 'birmingham') {
+        res.redirect('/vehicle-checker/birmingham/enter-vehicle-details')
+    } else {
+        res.redirect('/vehicle-checker/local-authority')
+    }
+});
+
 module.exports = router
