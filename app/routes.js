b216b6dca0b0cb7319628740c2e6a90eb059f143
Rica Duchateau <rica.duchateau@informed.com>
2018-10-26 14:58:48 +0100

    Fix routing issue

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,13 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 router.post('/vehicle-checker/local-authority', function (req, res) {
-    var localAuthority = req.body['local-authority'];
+   var localAuthority = req.body['local-authority'];
 
-    if (localAuthority == 'birmingham') {
-        res.redirect('/vehicle-checker/birmingham/enter-vehicle-details')
-    } else {
-        res.redirect('/vehicle-checker/local-authority')
-    }
+   if (localAuthority == 'birmingham') {
+       res.redirect('/vehicle-checker/birmingham/enter-vehicle-details')
+   } else {
+       res.redirect('/vehicle-checker/discovery')
+   }
 });
 
 module.exports = router
