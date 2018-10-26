e9725f0ff110aae42214047cf9c151952fc036aa
Rica Duchateau <rica.duchateau@informed.com>
2018-10-26 15:03:11 +0100

    Add Leeds and Southampton options to routes.js

--- app/routes.js
+++ app/routes.js
@@ -4,13 +4,15 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 router.post('/vehicle-checker/local-authority', function (req, res) {
-   var localAuthority = req.body['local-authority'];
+    var localAuthority = req.body['local-authority'];
 
-   if (localAuthority == 'birmingham') {
-       res.redirect('/vehicle-checker/birmingham/enter-vehicle-details')
-   } else {
-       res.redirect('/vehicle-checker/discovery')
-   }
+    if (localAuthority == 'birmingham') {
+        res.redirect('/vehicle-checker/birmingham/enter-vehicle-details')
+    } else if (localAuthority == 'leeds') {
+        res.redirect('/vehicle-checker/leeds/enter-vehicle-details')
+    } else if (localAuthority == 'southampton') {
+        res.redirect('/vehicle-checker/southampton/enter-vehicle-details')
+    }
 });
 
 module.exports = router
