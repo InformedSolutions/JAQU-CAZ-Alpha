a2292db47b1c59c095f9831e82fa618127fbdfa0
Rob <robert.sinclair@informed.com>
2018-11-30 09:29:41 +0000

    back buttons and summary page

--- app/routes.js
+++ app/routes.js
@@ -115,4 +115,46 @@ res.render('vehicle-checker/compliancePage', {
 });
 });
 
+router.get('/vehicle-checker/summary', function (req, res) {
+  // Complete tag
+  if (req.session.data['caz-1'] == "birmingham"){
+    req.session.birminghamShow = true;
+  } else {
+    req.session.birminghamShow = false;
+  }
+
+  if (req.session.data['caz-2'] == "derby"){
+    req.session.derbyShow = true;
+  } else {
+    req.session.derbyShow = false;
+  }
+
+  if (req.session.data['caz-3'] == "leeds"){
+    req.session.leedsShow = true;
+  } else {
+    req.session.leedsShow = false;
+  }
+
+  if (req.session.data['caz-4'] == "nottingham"){
+    req.session.nottinghamShow = true;
+  } else {
+    req.session.nottinghamShow = false;
+  }
+
+  if (req.session.data['caz-5'] == "southampton"){
+    req.session.southamptonShow = true;
+  } else {
+    req.session.southamptonShow = false;
+  }
+
+
+res.render('vehicle-checker/summary', {
+  optional: true,
+  birminghamShow: req.session.birminghamShow,
+  leedsShow: req.session.leedsShow,
+  derbyShow: req.session.derbyShow,
+  nottinghamShow: req.session.nottinghamShow,
+  southamptonShow: req.session.southamptonShow
+});
+});
 module.exports = router
