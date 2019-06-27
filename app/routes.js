1ce83472d8f1f667883937aa467b56c54c1a114c
Thomas-95IS <thomas.stokes@informed.com>
2019-06-27 17:10:34 +0100

    Add two pages for CSV upload interface.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.get('/upload', function (req, res) {
+  res.render('upload', {});
+});
+
+router.get('/upload-error', function (req, res) {
+  res.render('upload-error', {});
+});
 
 module.exports = router
